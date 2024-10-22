5199494b79839d6ccdde1ff1a401561246064aa3
andr7w <afouetieu@gmail.com>
2024-10-21 23:27:37 -0400

    add spring initial application

---
+++ src/main/java/com/pocmaster/demo/Kafka2LogstashApp.java
@@ -0,0 +1,13 @@
+package com.pocmaster.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka2LogstashApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafka2LogstashApp.class, args);
+	}
+
+}
