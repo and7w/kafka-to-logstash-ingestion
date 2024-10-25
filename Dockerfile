c3f8e8875525f2886bbfdc7cd85f060539395a88
andr7w <afouetieu@gmail.com>
2024-10-25 00:34:43 -0400

    add docker-compose for the java application

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM eclipse-temurin:21-jre
+
+ARG JAR_FILE=target/kafka-to-logstash-ingestion-1.0.2.jar
+COPY ${JAR_FILE} /app.jar
+EXPOSE 8081
+ENTRYPOINT ["java", "-jar", "app.jar"]
+
---
+++ src/main/java/com/pocmaster/demo/GreetingController.java
@@ -0,0 +1,13 @@
+package com.pocmaster.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/salutation")
+    public String getMessage() {
+        return "Thank you...POCMaster man";
+    }
+}
